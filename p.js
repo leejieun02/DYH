9de69e8827c8d281ff42f3a535b06462003c92df
leejieun02 <77762508+leejieun02@users.noreply.github.com>
2023-11-03 23:51:30 +0900

    Update p.js

--- p.js
+++ p.js
@@ -1,4 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('payment-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -12,29 +12,51 @@
         // 결제 성공 후 메인 페이지로 리디렉션
         window.location.href = 'main.html'; // 메인 페이지의 URL로 변경하세요.
     });
-});*/
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const productSelect = document.getElementById('product');
-    const quantityInput = document.getElementById('quantity');
-    const totalPriceElement = document.getElementById('total-price');
-
-    // 상품 선택 또는 수량 입력 시 총 가격 업데이트
-    function updateTotalPrice() {
-        const selectedProduct = productSelect.options[productSelect.selectedIndex];
-        const productPrice = parseFloat(selectedProduct.getAttribute('data-price'));
-        const quantity = parseInt(quantityInput.value);
-        const total = productPrice * quantity;
-
-        // 총 가격을 표시
-        totalPriceElement.textContent = `₩${total.toFixed(0)}`;
-    }
-
-    productSelect.addEventListener('change', updateTotalPrice);
-    quantityInput.addEventListener('input', updateTotalPrice);
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const checkoutButton = document.getElementById('checkoutButton');
+    const paymentMethodSelect = document.getElementById('payment-method');
 
+    checkoutButton.addEventListener('click', function () {
+        const selectedPaymentMethod = paymentMethodSelect.value;
 
+        switch (selectedPaymentMethod) {
+            case 'credit-card':
+                handleCreditCardPayment();
+                break;
+            case 'bank-transfer':
+                handleBankTransfer();
+                break;
+            case 'kakao-pay':
+                handleKakaoPay();
+                break;
+            case 'naver-pay':
+                handleNaverPay();
+                break;
+            default:
+                alert('결제 방법을 선택하세요.');
+        }
+    });
+
+    function handleCreditCardPayment() {
+        // 여기에 신용카드 결제 처리 로직을 추가합니다.
+        alert('신용카드로 결제합니다.');
+    }
+
+    function handleBankTransfer() {
+        // 여기에 가상계좌 결제 처리 로직을 추가합니다.
+        alert('가상계좌로 결제합니다.');
+    }
+
+    function handleKakaoPay() {
+        // 여기에 카카오페이 결제 처리 로직을 추가합니다.
+        alert('카카오페이로 결제합니다.');
+    }
+
+    function handleNaverPay() {
+        // 여기에 네이버페이 결제 처리 로직을 추가합니다.
+        alert('네이버페이로 결제합니다.');
+    }
+});
